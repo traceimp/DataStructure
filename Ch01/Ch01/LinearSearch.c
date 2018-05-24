5079cad986dd8bc8ad3ac6aa4873d0a7f4a09e90
혜진 김 <traceimp@outlook.kr>
2018-05-24 17:33:22 +0900

    챕터1 > 순환 구조 예제

---
+++ Ch01/Ch01/LinearSearch.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int LSearch(int ar[], int len, int target)	// 순자 탐색 알고리즘을 적용한 함수
+{
+	int i;
+	for (i = 0; i < len; i++)
+	{
+		if (ar[i] == target)
+		{
+			return i;	// 찾은 대상의 인덱스 값 반환
+		}
+	}
+	return -1;
+}
+
+int main(void)
+{
+	int arr[] = { 3, 5, 2, 4, 9 };
+	int idx;
+
+	idx = LSearch(arr, sizeof(arr) / sizeof(int), 4);
+	if (idx == -1)
+		printf("탐색 실패\n");
+	else
+		printf("타겟 저장 인덱스: %d \n", idx);
+
+	idx = LSearch(arr, sizeof(arr) / sizeof(int), 7);
+	if(idx == -1)
+		printf("탐색 실패\n");
+	else
+		printf("타겟 저장 인덱스: %d \n", idx);
+
+	return 0;
+
+}
